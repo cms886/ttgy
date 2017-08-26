9ce3f58a6e8788392b37663b70979f9669acbb5e
ttgy <ming9191@126.com>
2017-08-26 23:32:37 +0800

    ssss

--- tiantian/goods/admin.py
+++ tiantian/goods/admin.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
 from .models import TypeInfo,GoodsInfo
@@ -8,7 +9,7 @@ class TypeInfoAdmin(admin.ModelAdmin):
 class GoodsInfoAdmin(admin.ModelAdmin):
 	list_per_page = 15
 	list_display = ['id', 'gtitle','gdigest','gprice','gkucun','gunit','gtime','gclick','isDetele','ginfo','gtj']
-
+#把 新增加的 TypeInfoAdmin 也注册进来
 admin.site.register(TypeInfo,TypeInfoAdmin)
 admin.site.register(GoodsInfo,GoodsInfoAdmin)
 
--- tiantian/goods/views.py
+++ tiantian/goods/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from .models import  GoodsInfo,GoodsInfo
 from django.http import HttpResponse
 
 def index(request):
+    
     return render(request,'index.html')
 
