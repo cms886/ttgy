563cc312913f4a34e6c10bf8c9aa6ce93e37dfb4
ttgy <ming9191@126.com>
2017-08-26 16:29:33 +0800

    sss

--- tiantian/goods/models.py
+++ tiantian/goods/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from tinymce.models import  HTMLField
+#添加 商品 类别
+class TypeInfo(models.Model):
+	title = models.CharField(max_length=30)
+	isDelete = models.BooleanField(default=False)  #逻辑删除
 
-# Create your models here.
+# 商品表
+class GoodsInfo(models.Model):
+	gtype= models.ForeignKey(TypeInfo) #类
+	gtitle = models.CharField(max_length=20)      #标题
+	gimage = models.ImageField(upload_to='goods') #上传目录
+	gprice = models.DecimalField(max_digits=5,decimal_places=2)     #价格
+	gunit = models.CharField(max_length=20)  #单位500g
+	gtime = models.DateTimeField() # 上架 时间
+	gclick = models.IntegerField()  # 人气|点击 点击量
+	isDetele = models.BooleanField(default=False) #逻辑删除
+	gdigest = models.CharField(max_length=100)  # 摘要
+	ginfo = HTMLField()  #商品介绍 文本 编辑器 
+	gkucun = models.IntegerField()  #库存
+	gtj = models.BooleanField(default=False)  # 新品推荐
+    
