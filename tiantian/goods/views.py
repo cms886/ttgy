682ecd3a31ed785321c18aad2961088b74956d17
ttgy <ming9191@126.com>
2017-08-27 00:55:35 +0800

    s

--- tiantian/goods/views.py
+++ tiantian/goods/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import TypeInfo,GoodsInfo
 def index(request):
-    
-    return render(request,'index.html')
-
+    #商品类
+    goods = TypeInfo.objects.all().order_by()[:6]  # 取出最新信息前4条
+    #商品名称
+    post_list = GoodsInfo.objects.all().order_by('-gtime')[:4]   #取出最新信息前4条
+    return render(request,'index.html',context={'post_list':post_list,'goods':goods})
 
 def detail(request):
     return render(request,'detail.html')
