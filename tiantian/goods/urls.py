48029268ed8f8a1b2e14ca1498ac4698f7c2afd0
ttgy <ming9191@126.com>
2017-08-27 13:14:58 +0800

    s

--- tiantian/goods/urls.py
+++ tiantian/goods/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+app_name = 'goods'
 urlpatterns = [
     url(r'admin/',admin.site.urls),
     url(r'^$',views.index,name="index"),
-    url(r'^de/$',views.detail,name="detail")
+    # url('^de/$',views.detail,name='detail'),
+    url('^detail/(?P<id>[0-9]+)/$',views.detail,name='detail'),
 ]
--- tiantian/goods/views.py
+++ tiantian/goods/views.py
@@ -9,26 +9,26 @@ def index(request):
     #post_list = GoodsInfo.objects.all().order_by('-gtime')[:4]   #取出最新信息前4条
     # 楼层
     p = TypeInfo.objects.all()[0]
-    xxsg = p.goodsinfo_set.all().order_by('-gtime')[:4]
+    xxsg = p.goodsinfo_set.all().order_by('gtime')[:4]
     #海鲜水产
     type = TypeInfo.objects.all()[1]
-    hxsc = type.goodsinfo_set.all().order_by('-gtime')[:4]
+    hxsc = type.goodsinfo_set.all().order_by('gtime')[:4]
     #猪肉 牛羊
     r = TypeInfo.objects.all()[2]
-    zrny = r.goodsinfo_set.all().order_by('-gtime')[:4]
+    zrny = r.goodsinfo_set.all().order_by('gtime')[:4]
     #禽类蛋品
     dan = TypeInfo.objects.all()[3]
-    qldp = dan.goodsinfo_set.all().order_by('-gtime')[:4]
+    qldp = dan.goodsinfo_set.all().order_by('gtime')[:4]
     #新鲜蔬菜
     sc = TypeInfo.objects.all()[4]
-    xxsc = sc.goodsinfo_set.all().order_by('-gtime')[:4]
+    xxsc = sc.goodsinfo_set.all().order_by('gtime')[:4]
     #速冻食品
     sp =TypeInfo.objects.all()[5]
-    sdsp = sp.goodsinfo_set.all().order_by('-gtime')[:4]
+    sdsp = sp.goodsinfo_set.all().order_by('gtime')[:4]
 
     return render(request,'index.html',context={'goods':goods,'xxsg':xxsg,'hxsc':hxsc,'zrny':zrny,'qldp':qldp,'xxsc':xxsc,'sdsp':sdsp})
 
 
 
-def detail(request):
+def detail(request,id):
     return render(request,'detail.html')
