382f66fe90907eed1a2c578d2f003248922f9001
ttgy <ming9191@126.com>
2017-08-28 11:52:37 +0800

    s

--- tiantian/df_cart/models.py
+++ tiantian/df_cart/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
 class CartInfo(models.Model):
-    user = models.Foreignkey('goods.GoodsInfo')  #这是 多对多 一个用户对应 多个商品
-    goods = models.Foreignkey('df_user.UserInfo') #  用户表
+    user = models.ForeignKey('goods.GoodsInfo')  #这是 多对多 一个用户对应 多个商品
+    goods = models.ForeignKey('df_user.UserInfo') #  用户表
     count = models.IntegerField()
 
 # Create your models here.
