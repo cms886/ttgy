78680170f348cc619ec2d794e476cbd05fb269d5
ttgy <ming9191@126.com>
2017-08-28 22:42:37 +0800

    s

--- tiantian/goods/views.py
+++ tiantian/goods/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import JsonResponse,HttpResponse
 from .models import TypeInfo,GoodsInfo
-from django.core.paginator import Paginator,EmptyPage
+from django.core.paginator import Paginator,EmptyPage ,PageNotAnInteger
 def index(request):
     #商品类
     goods = TypeInfo.objects.all().order_by()[:6]  # 取出最新信息前6条
@@ -36,11 +36,17 @@ def detail(request,id):
 
 
 #全部list
-def goodslist(request, pIndex):
+def goodslist(request,pIndex,bq):
     #新品推荐
     tj = GoodsInfo.objects.filter(gtj=True).order_by('-gtime')[:2]
     #分页显示
-    list1 = GoodsInfo.objects.all()
+    biaoqian = bq
+    if biaoqian == "1":#默认
+        list1 = GoodsInfo.objects.all().order_by('-gtime')
+    elif biaoqian == "2":#价格
+        list1 = GoodsInfo.objects.all().order_by('-gprice')
+    else: #点击
+        list1 = GoodsInfo.objects.all().order_by('-gclick')
     p = Paginator(list1,15)
     if pIndex == '':
         pIndex = '1'
@@ -48,22 +54,34 @@ def goodslist(request, pIndex):
     #取第几分页对象
     list2 = p.page(pIndex)
     plist = p.page_range
-    return render(request, 'list.html', {'list': list2, 'plist': plist, 'pIndex': pIndex,'tj':tj})
+    return render(request, 'list.html', {'list': list2, 'plist': plist, 'pIndex': pIndex,'tj':tj,'biaoqian':biaoqian})
 
-def typelist(request,typeid):
-    r = TypeInfo(id=typeid)
-    jgj = r.goodsinfo_set.all()
-    # paginator = Paginator(jgj, 15)  # Show 25 contacts per page
-    # page = request.GET.get('page')
-    # try:
-    #     contacts = paginator.page(page)
-    # except PageNotAnInteger:
-    #     contacts = paginator.page(1)
-    # except EmptyPage:
-    #     contacts = paginator.page(paginator.num_pages)
-
+#查询点击最高
+def hot(request):
+    # 新品推荐
+    tj = GoodsInfo.objects.filter(gtj=True).order_by('-gtime')[:2]
+    #分页
+    jgj = GoodsInfo.objects.all().order_by('-gclick')
+    paginator = Paginator(jgj, 15)
+    page = request.GET.get('page')
+    try:
+         contacts = paginator.page(page)
+    except PageNotAnInteger:
+         contacts = paginator.page(1)
+    except EmptyPage:
+         contacts = paginator.page(paginator.num_pages)
     # 取第几分页对象
-    return render(request, 'typelist.html', {'list': jgj})
+    return render(request, 'typelist.html', {'contacts': contacts,'tj':tj})
+
+def typelist(request,typeid,bq):
+    r = TypeInfo(id=typeid)
+    if bq == '1':
+        jgj = r.goodsinfo_set.all().order_by('-gtime')
+    elif bq == '2':
+        jgj = r.goodsinfo_set.all().order_by('-gprice')
+    else:
+        jgj = r.goodsinfo_set.all().order_by('-gclick')
+    return render(request, 'typelist.html', {'list': jgj,'typeid':typeid})
 
 
 
