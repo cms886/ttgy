655bd1bcaf683c43f5ed4906fd6e8fb8de76144a
lichong <2224268262@qq.com>
2017-08-29 03:20:06 +0800

    s

--- tiantian/df_cart/views.py
+++ tiantian/df_cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
 from df_user.models import  UserInfo
+from goods.models import GoodsInfo
 from .models import  CartInfo
 
 
@@ -10,9 +11,32 @@ def gwc(request):
 		gid = request.POST.get('gid')
 		gcount = request.POST.get('gcount')
 		uname = request.COOKIES["uname"]
+		print(uname)
 		pp = UserInfo.objects.filter(uname=uname)[0]
 		uid = pp.id
 		obj =CartInfo(goods = gid,user = uid ,count = gcount)
 		obj.save()
-		return HttpResponse('ok')
+		return HttpResponse('add ok')
+
+def showcar(request):
+	uname = request.COOKIES["uname"]
+	pp = UserInfo.objects.filter(uname=uname)[0]
+	uid = pp.id
+	obj = CartInfo.objects.filter(user=uid)
+	glist = []
+	for i in obj:
+		gid = i.goods
+		t = i.count
+		g = GoodsInfo.objects.filter(id=gid)[0]
+		title = g.gtitle
+		image = g.gimage
+		unit = g.gunit
+		price = g.gprice
+		glist.append(title)
+		glist.append(image)
+		glist.append(unit)
+		glist.append(price)
+		glist.append(t)
+	return render(request,'cart.html',{'list':glist,'t':t})
+
 
