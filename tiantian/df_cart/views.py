7ffe8b29d86e17232a5e803b5501af90e4072d86
lichong <2224268262@qq.com>
2017-09-01 10:01:40 +0800

    asda

--- tiantian/df_cart/views.py
+++ tiantian/df_cart/views.py
@@ -1,50 +1,86 @@
-from django.shortcuts import render
-from django.http import  HttpResponse
+from django.shortcuts import render,redirect
+from django.http import  HttpResponse,JsonResponse
 from df_user.models import  UserInfo
 from goods.models import GoodsInfo
 from .models import  CartInfo
 
+def cart(request):
+	uid = request.session['user_id']
 
-# Create your views here.
+	carts = CartInfo.objects.filter(user_id = uid)
+	return render(request,'cart.html')
+
+def add(request,gid,count):
+	uid = request.session['user_id']
+
+	gid = int(gid)
+	count = int(count)
+	carts = CartInfo.objects.filter(user_id=uid,goods_id=gid)
+	if len(carts)>=1:
+		cart = carts[0]
+		cart.count = cart.count+count
+	else:
+		cart = CartInfo()
+		cart.user_id = uid
+		cart.goods_id = gid
+		cart.count = count
+
+	if request.is_ajax():
+		count = CartInfo.objects.filter(user_id=request.session['user_id']).count()
+		return JsonResponse({'count':count})
+	else:
+		return redirect('/cart/')
+def delete(request,cart_id):
+	try:
+	# 拿到删除的商品的ID
+		cart = CartInfo.objects.get(pk = int(cart_id))
+		cart.delete()
+		data={'ok':1}
+	except Exception as e:
+		data={'ok':0}
+	return JsonResponse(data)
+
+def addnum(request):
+	cid = request.POST.get('id')
+	num = request.POST.get('s')
+	cart = CartInfo.objects.filter(id=int(cid)).update(count=int(num))
+	return HttpResponse('ok')
+
+
 def gwc(request):
 	if request.method == 'POST':
 		gid = request.POST.get('gid')
 		gcount = request.POST.get('gcount')
-		uname = request.COOKIES["uname"]
-		pp = UserInfo.objects.filter(uname=uname)[0]
-		uid = pp.id
-		print(uid,gid,gcount)
-		#多对多
-		u = UserInfo.objects.get(id=uid)
-		g = GoodsInfo.objects.get(id=gid)
-		s = CartInfo(goods = g,user = u,count=gcount).save()
+		gcount= int(gcount)
+		uid = request.session["user_id"]
+		print(uid)
+		carts = CartInfo.objects.filter(user_id=uid, goods_id=gid)
+
+		if len(carts) >= 1:
+			cart = carts[0]
+			cart.count = cart.count + gcount
+		else:
+			cart = CartInfo()
+			cart.user_id = uid
+			cart.goods_id = gid
+			cart.count = gcount
+
+		cart.save()
 		return HttpResponse('添加购物车成功')  #添加 信息重复数量+1
 
 def showcar(request):
-	# uname = request.COOKIES["uname"]
-	# pp = UserInfo.objects.filter(uname=uname)[0]
-	# uid = pp.id
-	# obj = CartInfo.objects.filter(user=uid)
-	# glist = []
-	# for i in obj:
-	# 	gid = i.goods
-	# 	t = i.count
-	# 	g = GoodsInfo.objects.filter(id=gid)[0]
-	# 	title = g.gtitle
-	# 	image = g.gimage
-	# 	unit = g.gunit
-	# 	price = g.gprice
-	# 	glist.append(title)
-	# 	glist.append(image)
-	# 	glist.append(unit)
-	# 	glist.append(price)
-	# 	glist.append(t)
-
-
 	uname = request.COOKIES["uname"]
 	pp = UserInfo.objects.filter(uname=uname)[0]
 	uid = pp.id
-	list = CartInfo.objects.filter(user_id=uid)
-	return render(request,'cart.html',{'list':list})
+	ee = CartInfo.objects.filter(user_id = uid)
+	lena = len(ee)
+	for i in ee:
+		c = i.count
+		print(i.count)
+
+
+	# list = CartInfo.objects.filter(user_id=uid)
+
+	return render(request,'cart.html',{'list':ee,'leng':lena})
 
 
