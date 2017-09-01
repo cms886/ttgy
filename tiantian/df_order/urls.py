bb5e3df543191819fd2247f8cfe5372afdf5e328
ttgy <ming9191@126.com>
2017-09-01 10:41:10 +0800

    s

---
+++ tiantian/df_order/urls.py
@@ -0,0 +1,8 @@
+from  django.conf.urls import  url
+from django.contrib import admin
+from . import views
+app_name = 'df_order'
+urlpatterns = [
+    url(r'admin/',admin.site.urls),
+    url(r'^place/$',views.place,name="placr"),
+]
--- tiantian/df_order/views.py
+++ tiantian/df_order/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+from  df_cart.models import  CartInfo
+from goods.models import GoodsInfo
 # Create your views here.
+
+#支付页
+def place(request):
+	#获取登录用户id
+	#order = request.GET.getlist('cart_id')
+	uid = request.session['user_id']
+	carts = CartInfo.objects.filter(user_id=uid)
+	context = {'carts':carts}
+	return render(request,'place_order.html',context)
+
