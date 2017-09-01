c8eec43bce3b7cb5c194dc9af1b0ca0cc167d1c9
lichong <2224268262@qq.com>
2017-09-01 10:02:18 +0800

    asd

--- tiantian/df_cart/urls.py
+++ tiantian/df_cart/urls.py
@@ -6,5 +6,12 @@ app_name = 'df_cart'
 urlpatterns = [
     url(r'admin/',admin.site.urls),
     url(r'^gwc/$',views.gwc,name="gwc"),
-    url(r'^showcar/$',views.showcar,name="showcar")
+    url(r'^showcar/$',views.showcar,name="showcar"),
+
+    url(r'^$',views.cart),
+    url(r'^add(\d+)_(\d+)/$',views.add),
+    # url(r'^edit(\d+)_(\d+)/$',views.edit),
+    url(r'^delete(\d+)/$',views.delete),
+    url(r'^addnum/$',views.addnum),
+
 ]
